6cbed6e17707cad36b136cd46a4b1ed1d1f51cf3
George <gs.ivanov@abv.bg>
2025-03-21 08:36:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Gpt_Turnir.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
